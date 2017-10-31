1f46540e226b8678f270a8d12c818e218d14d024
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2017-10-31 13:31:51 +0800

    test from phpmailer

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -1,4 +1,7 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 class Cron extends CI_Controller
 {
     public function createTree()
@@ -129,5 +132,50 @@ class Cron extends CI_Controller
         ];
         $mail_conf = $this->mail_conf->get($id);
         $this->appnotify->test_send($mail_conf,$mail);
+    }
+
+    public function test_send_3()
+    {
+
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer;
+        //Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+        //Enable SMTP debugging
+        // 0 = off (for production use)
+        // 1 = client messages
+        // 2 = client and server messages
+        $mail->SMTPDebug = 2;
+        //Set the hostname of the mail server
+        $mail->Host = 'postmaster.1govuc.gov.my';
+        //Set the SMTP port number - likely to be 25, 465 or 587
+        $mail->Port = 25;
+        //Whether to use SMTP authentication
+        $mail->SMTPAuth = FALSE;
+        //Username to use for SMTP authentication
+        $mail->Username = 'yourname@example.com';
+        //Password to use for SMTP authentication
+        $mail->Password = 'yourpassword';
+        //Set who the message is to be sent from
+        $mail->setFrom('pentadbirijknm.moh@1govuc.gov.my', 'ijknm');
+        //Set an alternative reply-to address
+        $mail->addReplyTo('pentadbirijknm.moh@1govuc.gov.my', 'ijknm');
+        //Set who the message is to be sent to
+        $mail->addAddress('md.ridzuan80@gmail.com', 'John Doe');
+        //Set the subject line
+        $mail->Subject = 'PHPMailer SMTP test';
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        $mail->msgHTML($this->load->view("layout/email/pengujian",'',TRUE));
+        //Replace the plain text body with one created manually
+        $mail->AltBody = 'This is a plain-text message body';
+        //Attach an image file
+        //$mail->addAttachment('images/phpmailer_mini.png');
+        //send the message, check for errors
+        if (!$mail->send()) {
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message sent!';
+        }
     }
 }
