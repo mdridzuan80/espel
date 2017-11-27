e52d9fd67074dfe9dd030ad9ac8e6f98d3bd5382
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2017-11-28 06:06:51 +0800

    Fix laporan Jabatan x jawab

--- application/models/Profil_model.php
+++ application/models/Profil_model.php
@@ -49,7 +49,7 @@ class Profil_model extends MY_Model
             hrmis_kumpulan.keterangan AS kumpulan,
             hrmis_carta_organisasi.title AS jabatan
             FROM
-            view_laporan_statistik_prestasi as espel_profil
+            espel_profil
             INNER JOIN hrmis_carta_organisasi ON espel_profil.jabatan_id = hrmis_carta_organisasi.buid
             INNER JOIN hrmis_kumpulan ON espel_profil.kelas_id = hrmis_kumpulan.kod
             INNER JOIN hrmis_skim ON hrmis_skim.kod = espel_profil.skim_id
