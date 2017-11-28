65b116aa265200efcf66bcba95c25a52b4348935
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2017-11-29 05:33:31 +0800

    disallow double click for mohon button at permohona kursus by anggota

--- application/views/kursus/pengguna/js.php
+++ application/views/kursus/pengguna/js.php
@@ -2,7 +2,8 @@
 $(function(){
     $('#btnMohon').on('click', function(e) {
         e.preventDefault();
-        var kursus_id = $(this).attr('data-kursus_id');
+        var el = $(this);
+        var kursus_id = el.attr('data-kursus_id');
         swal({
             title: 'Anda Pasti?',
             type: 'warning',
@@ -15,6 +16,7 @@ $(function(){
             cancelButtonClass: 'btn btn-danger',
             buttonsStyling: false
         }).then(function () {
+            el.attr('disabled',true);
             $.ajax({
                 url: base_url + 'api/csrf',
                 success: function(data, textStatus, jqXHR ) {
@@ -44,11 +46,8 @@ $(function(){
             // dismiss can be 'cancel', 'overlay',
             // 'close', and 'timer'
             if (dismiss === 'cancel') {
-                swal(
-                'Batal!',
-                '',
-                'error'
-                )
+                swal('Batal!','','error');
+                el.attr('disabled',false);
             }
         });
     });
