154e851f1b4abf625d23eca218922a035a96a8f7
Md Ridzuan bin Mohammad Latiah <mdridzuan@melaka.gov.my>
2018-10-19 10:33:26 +0800

    rename index.php to default.php coz git ignore

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,6 +5,6 @@ class Welcome extends CI_Controller
 {
 	public function index()
 	{
-		return $this->load->view('landing/index');
+		return $this->load->view('landing/default');
 	}
 }
---
+++ application/views/landing/default.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Sistem Pengurusan Latihan eSPeLv2.0</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css" />
+
+	<!-- Owl Carousel -->
+	<link type="text/css" rel="stylesheet" href="<?= base_url() ?>assets/css/owl.carousel.css" />
+	<link type="text/css" rel="stylesheet" href="<?= base_url() ?>assets/css/owl.theme.default.css" />
+
+	<!-- Magnific Popup -->
+	<link type="text/css" rel="stylesheet" href="<?= base_url() ?>assets/css/magnific-popup.css" />
+
+	<!-- Font Awesome Icon -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/font-awesome.min.css">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="<?= base_url() ?>assets/css/style.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Header -->
+	<header id="home">
+		<!-- Background Image -->
+		<div class="bg-img" style="background-image: url('./assets/img/background1.jpg');">
+			<div class="overlay"></div>
+		</div>
+		<!-- /Background Image -->
+
+		<!-- Nav -->
+		<nav id="nav" class="navbar nav-transparent">
+			<div class="container">
+
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a href="index.html">
+							<img class="logo" src="<?= base_url() ?>assets/img/logo.png" alt="logo">
+							<img class="logo-alt" src="<?= base_url() ?>assets/img/logo-alt.png" alt="logo">
+						</a>
+					</div>
+					<!-- /Logo -->
+
+					<!-- Collapse nav button -->
+					<div class="nav-collapse">
+						<span></span>
+					</div>
+					<!-- /Collapse nav button -->
+				</div>
+
+				<!--  Main navigation  -->
+				<ul class="main-nav nav navbar-nav navbar-right">
+					<li><a href="#home">Utama</a></li>
+					<li><a href="#about">Tentang Kami</a></li>
+					<li><a href="#contact">Hubungi Kami</a></li>
+				</ul>
+				<!-- /Main navigation -->
+
+			</div>
+		</nav>
+		<!-- /Nav -->
+
+		<!-- home wrapper -->
+		<div class="home-wrapper">
+			<div class="container">
+				<div class="row">
+
+					<!-- home content -->
+					<div class="col-md-10 col-md-offset-1">
+						<div class="home-content">
+							<h1 class="white-text">Sistem Pengurusan Latihan eSPeL v2.0</h1>
+							<p class="white-text" style="text-transform: uppercase;">Jabatan Kesihatan Negeri Melaka
+							</p>
+							<button class="main-btn">Masuk!</button>
+						</div>
+					</div>
+					<!-- /home content -->
+
+				</div>
+			</div>
+		</div>
+		<!-- /home wrapper -->
+
+	</header>
+	<!-- /Header -->
+
+	<!-- About -->
+	<div id="about" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<h2 class="title">Selamat Datang</h2>
+				</div>
+				<!-- /Section header -->
+
+				<!-- about -->
+				<div class="col-md-4">
+					<div class="about">
+						<i class="fa fa-cogs"></i>
+						<h3>Pelbagai Laporan</h3>
+						<p>Menjana dan Memaparkan laporan untuk pelbagai tujuan</p>
+					</div>
+				</div>
+				<!-- /about -->
+
+				<!-- about -->
+				<div class="col-md-4">
+					<div class="about">
+						<i class="fa fa-magic"></i>
+						<h3>Fungsi</h3>
+						<p>Pelbagai fungsi seperti prorate kelayakan berkursus</p>
+					</div>
+				</div>
+				<!-- /about -->
+
+				<!-- about -->
+				<div class="col-md-4">
+					<div class="about">
+						<i class="fa fa-mobile"></i>
+						<h3>Responsif</h3>
+						<p>Paparan yang mesra dengan peralatan yang digunakan</p>
+					</div>
+				</div>
+				<!-- /about -->
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /About -->
+
+	<!-- Contact -->
+	<div id="contact" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section-header -->
+				<div class="section-header text-center">
+					<h2 class="title">Hubungi Kami</h2>
+				</div>
+				<!-- /Section-header -->
+
+				<!-- contact -->
+				<div class="col-sm-4">
+					<div class="contact">
+						<i class="fa fa-phone"></i>
+						<h3>Phone/Fax</h3>
+						<p>Phone : 06-235 6999</p>
+						<p>Fax : 06-234 5969</p>
+					</div>
+				</div>
+				<!-- /contact -->
+
+				<!-- contact -->
+				<div class="col-sm-4">
+					<div class="contact">
+						<i class="fa fa-envelope"></i>
+						<h3>Email</h3>
+						<p>jkesihatan@moh.gov.my</p>
+						<p>https://jknmelaka.moh.gov.my</p>
+					</div>
+				</div>
+				<!-- /contact -->
+
+				<!-- contact -->
+				<div class="col-sm-4">
+					<div class="contact">
+						<i class="fa fa-map-marker"></i>
+						<h3>Address</h3>
+						<p>Aras 4, Wisma Persekutuan Jalan Business City, Bandar MITC
+Ayer Keroh</p>
+					</div>
+				</div>
+				<!-- /contact -->
+				
+			</div>
+			<!-- /Row -->
+			<div style="height: 200px;">&nbsp;</div>
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Contact -->
+
+	<!-- Back to top -->
+	<div id="back-to-top"></div>
+	<!-- /Back to top -->
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div class="preloader">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<!-- /Preloader -->
+
+	<!-- jQuery Plugins -->
+	<script type="text/javascript" src="<?= base_url() ?>assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/js/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/js/main.js"></script>
+	<script>
+		$('.main-btn').on('click', function(e){
+			document.location = '<?= base_url('login') ?>';
+		});
+	</script>
+
+</body>
+
+</html>
