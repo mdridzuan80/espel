cfae6987c259686d8c24e2ace32e8088f614dce7
Md Ridzuan bin Mohammad Latiah <mdridzuan@melaka.gov.my>
2018-11-21 11:15:28 +0800

    Fix privlleges

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,4 +7,12 @@ class Welcome extends CI_Controller
 	{
 		return $this->load->view('landing/default');
 	}
+
+	public function get_tree_jabatan()
+	{
+		$this->load->model("hrmis_carta_model", "jabatan");
+		$this->output
+			->set_content_type('application/json')
+			->set_output(json_encode(buildTreeParentInc($this->jabatan->as_array()->get_all(), 6792, 6792)));
+	}
 }
