e15d9383586cc3f94ab1e8f0cda70ec3371ac92b
Md Ridzuan bin Mohammad Latiah <mdridzuan@melaka.gov.my>
2018-11-21 11:21:40 +0800

    fix jabatan tree related

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -15,4 +15,20 @@ class Welcome extends CI_Controller
 			->set_content_type('application/json')
 			->set_output(json_encode(buildTreeParentInc($this->jabatan->as_array()->get_all(), 6792, 6792)));
 	}
+
+	public function get_tree_jabatan_related()
+	{
+		$this->load->model("hrmis_carta_model", "jabatan");
+		$this->load->model('kumpulan_profil_model', 'kumpulan_profil');
+
+		if ($this->appsess->getSessionData("kumpulan") == Appauth::SUPER || $this->appsess->getSessionData("kumpulan") == Appauth::ADMIN) {
+			$id = $this->config->item('espel_default_jabatan_id');
+		} else {
+			$id = $this->appsess->getSessionData('ptj_jabatan_id');;
+		}
+
+		$this->output
+			->set_content_type('application/json')
+			->set_output(json_encode(buildTreeParentInc($this->jabatan->as_array()->get_all(), $id, $id)));
+	}
 }
